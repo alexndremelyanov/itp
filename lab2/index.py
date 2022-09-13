1f82303fb39cd6db20692a07493fd82fa825bbca
a.emelyanov <alexndr.emelyanov@gmail.com>
2022-09-13 12:10:42 +0300

    feat(lab2): palindrome

---
+++ lab2/index.py
@@ -0,0 +1,13 @@
+def isPalindrome(str):
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
+
+
+def test(v):
+    print(f"{v} is palindrome: {isPalindrome(v)}")
+
+
+test("abba")
+test('text')
