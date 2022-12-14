335acc597c408f357ceb8d88e11826b743230a51
a.emelyanov <alexndr.emelyanov@gmail.com>
2022-12-14 20:28:18 +0300

    refactor(lab5): print table

--- lab5/index.py
+++ lab5/index.py
@@ -1,58 +1,55 @@
 #!/usr/bin/env python
-import gzip
-import io
 import os
 import re
+from beautifultable import BeautifulTable
 
-# log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
-#                     '$status $body_bytes_sent "$http_referer" '
-#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
-#                     '$request_time';
-
-config = {
-    "LOG_DIR": "./log"
-}
+default_logs_path = './log'
 
 
 def main():
-    log_names = sorted(os.listdir(config.get('LOG_DIR')))
-    if not log_names:
-        return print(f'Logs not found')
-    log_name = log_names[-1]
-    log_path = f'{config.get("LOG_DIR")}/{log_name}'
-    pattern = re.compile(r'\"[A-Z]+ (\S+) .* (\d+\.\d+)\n')
-    data = {}
+    relative_logs_path = input(f'Path to logs DIR [{default_logs_path}]: ')
+    if (relative_logs_path == ''):
+        relative_logs_path = default_logs_path
+    log_name = sorted(os.listdir(relative_logs_path))[-1]
+    print(log_name)
+    log_path = f'{relative_logs_path}/{log_name}'
+    with open(log_path, 'r') as file:
+        logs = read_logs(file)
+        print_logs(logs)
+
+
+def read_logs(file):
+    output = {}
     total_count = 0
-
-    def read_log(iterable_entity):
-        nonlocal total_count
-        for line in iterable_entity:
-            result = pattern.findall(line)
-            total_count += 1
-            if not len(result):
-                continue
-            url, time = result[0]
-            if url not in data:
-                data[url] = []
-            data[url].append(float(time))
-
-    if os.path.splitext(log_path)[1] == '.gz':
-        with gzip.open(log_path, 'r') as archive:
-            with io.TextIOWrapper(archive, encoding='utf-8') as decoder:
-                read_log(decoder)
-    else:
-        with open(log_path, 'r') as file:
-            read_log(file)
-    for key in data:
-        count = len(data[key])
-        print(f'URL: {key} \n'
-              f'\tcount: {count}'
-              f'\n\tcount_perc: {count/total_count * 100}'
-              f'\n\ttime_avg: {sum(data[key])/count}'
-              f'\n\ttime_max: {max(data[key])}'
-              f'\n\ttime_med: {data[key][count//2]}')
-        print('\n')
+    pattern = re.compile(r'\"[A-Z]+ (\S+) .* (\d+\.\d+)\n')
+    for line in file:
+        result = pattern.findall(line)
+        if not len(result):
+            continue
+        total_count += 1
+        url, time = result[0]
+        if not url in output:
+            output[url] = []
+        output[url].append(float(time))
+    return [output, total_count]
 
 
-if __name__ == "__main__":
+def print_logs(logs):
+    [logs, total_count] = logs
+    table = BeautifulTable()
+    table.columns.width = [40, 10, 10, 10, 10, 10]
+    table.columns.header = [
+        "url", "count", 'Count Per Sec', 'Time Average', "Time Max", 'Time Med']
+    for url in logs:
+        count = len(logs[url])
+        table.rows.append([url, str(count), str((count / total_count) * 100),
+                           str(sum(logs[url]) / count), str(max(logs[url])), str(logs[url][count // 2])])
+    table.rows.append([
+        "url", "count", 'Count Per Sec', 'Time Average', "Time Max", 'Time Med'])
+    print('Table Loading...')
+    print(table)
+    print('Total Logs Count: ', total_count)
+
+
+if __name__ == '__main__':
     main()
