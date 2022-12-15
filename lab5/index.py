ab29f22b34165a18badf85b382c8d350dbefada1
a.emelyanov <alexndr.emelyanov@gmail.com>
2022-12-15 13:39:59 +0300

    fix(lab6): mediate with statistics

--- lab5/index.py
+++ lab5/index.py
@@ -2,6 +2,7 @@
 import os
 import re
 from beautifultable import BeautifulTable
+import statistics
 
 default_logs_path = './log'
 
@@ -35,17 +36,17 @@ def read_logs(file):
 
 
 def print_logs(logs):
+    columns = [
+        "Url", "Count", 'Percent of All Logs', 'Time Average', 'Time Med', "Time Max"]
     [logs, total_count] = logs
     table = BeautifulTable()
     table.columns.width = [40, 10, 10, 10, 10, 10]
-    table.columns.header = [
-        "url", "count", 'Count Per Sec', 'Time Average', "Time Max", 'Time Med']
+    table.columns.header = columns
     for url in logs:
         count = len(logs[url])
-        table.rows.append([url, str(count), str((count / total_count) * 100),
-                           str(sum(logs[url]) / count), str(max(logs[url])), str(logs[url][count // 2])])
-    table.rows.append([
-        "url", "count", 'Count Per Sec', 'Time Average', "Time Max", 'Time Med'])
+        table.rows.append([url, count, (count * 100 / total_count),
+                           sum(logs[url]) / count, statistics.mean(logs[url]), max(logs[url])])
+    table.rows.append(columns)
     print('Table Loading...')
     print(table)
     print('Total Logs Count: ', total_count)
